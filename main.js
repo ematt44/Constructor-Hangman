58b3d0796dee725b9b2eee1a7662c2f993fe26f0
ematt44 <ericmatthews133@gmail.com>
2017-12-29 14:10:09 -0800

    use-prompt

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-var inquier = require ('inquirer');
+// Npm init to create package.json
+// Install prompt for user input in the command line
+// Create variable to require word.js file
+// Create variable to require letter.js file
+
+var prompt = require ('prompt');
+
+var word = require ('./word.js');
+
+var letter = require ('./letter.js');
+
+console.log ('hello');
